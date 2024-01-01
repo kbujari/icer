269101ca1d82fce3667f58efda296690ed01fc58
Kleidi Bujari <mail@kleidi.ca>
2023-12-31 23:14:00 -0500

    update: correct low pass filter and finish most of high pass

--- src/wavelet/mod.rs
+++ src/wavelet/mod.rs
@@ -1,13 +1,15 @@
 mod transform;
 
-use crate::{wavelet::transform::transform_1d, Icer};
+use self::transform::transform_1d;
+use crate::Icer;
 use image::{ImageBuffer, Luma};
 
 pub fn wavelet_transform(config: &Icer, data: &ImageBuffer<Luma<u8>, Vec<u8>>) {
-    let mut out: Vec<Vec<u32>> = Vec::new();
+    let mut out: Vec<Vec<u16>> = Vec::new();
+
     for row in data.rows() {
-        let input: Vec<u8> = row.map(|el| *el.0.first().unwrap()).collect();
-        out.push(transform_1d(input.as_slice(), &config.filter_params));
+        let input: Vec<u16> = row.map(|el| el.0[0] as u16).collect();
+        out.push(transform_1d(&input, &config.filter_params));
     }
 
     println!("{}", out.len());
--- src/wavelet/transform.rs
+++ src/wavelet/transform.rs
@@ -1,87 +1,79 @@
 use crate::FilterParams;
 
-fn low_pass_filter(data: &mut [u32], input: &[u8]) {
-    let odd = input.len() & 1 == 1;
-    let (last, elements) = data.split_last_mut().unwrap();
-
-    for (idx, el) in elements.iter_mut().enumerate() {
-        let n1 = input[2 * idx] as u32;
-        let n2 = input[(2 * idx) + 1] as u32;
-
-        *el = (n1 + n2) / 2;
-    }
-
-    *last = if odd {
-        *input.last().unwrap() as u32
-    } else {
-        let n1 = input[input.len() - 2] as u32;
-        let n2 = input[input.len() - 1] as u32;
-
-        (n1 + n2) / 2
-    };
-}
-
-fn high_pass_filter(data: &mut [u32], low_outputs: &[u32], input: &[u8], filter: &FilterParams) {
-    let odd = input.len() & 1 == 1;
-    let (a_neg, a0, a1, b) = filter.to_params();
-    let (last, elements) = data.split_last_mut().unwrap();
-
-    /* START PHASE 1 */
-
-    for (idx, el) in elements.iter_mut().enumerate() {
-        let n1 = input[2 * idx] as u32;
-        let n2 = input[(2 * idx) + 1] as u32;
-
-        *el = n1 - n2;
-    }
-
-    *last = if odd {
-        0
-    } else {
-        let n1 = input[input.len() - 2] as u32;
-        let n2 = input[input.len() - 1] as u32;
-
-        n1 - n2
-    };
-
-    let r: Vec<u32> = low_outputs
-        .iter()
-        .enumerate()
-        .skip(1)
-        .map(|(idx, _)| low_outputs[idx - 1] - low_outputs[idx])
-        .collect();
-
-    /* END PHASE 1 */
-
-    elements[0] -= r[1] / 4;
-
-    if *filter == FilterParams::C {
-        let n1 = r[1] / 4;
-        let n2 = r[2] * 3 / 8;
-        let n3 = elements[2] / 4;
-
-        elements[1] -= ((n1 + n2 - n3) as f32 + 0.5) as u32;
-    }
-
-    *last -= if odd {
-        let idx = input.len() - 1;
-        let n1 = a_neg * r[idx - 1] as f32;
-        let n2 = a0 * r[idx] as f32;
-        let n3 = a1 * r[idx + 1] as f32;
-        let n4 = b * elements[idx + 1] as f32;
-
-        (n1 + n2 + n3 - n4 + 0.5) as u32
-    } else {
-        r[input.len() / 2 - 1] / 4
-    };
-}
-
-pub fn transform_1d(input: &[u8], filter: &FilterParams) -> Vec<u32> {
-    let mut output: Vec<u32> = input.iter().copied().map(|el| el as u32).collect();
+pub fn transform_1d(input: &[u16], filter: &FilterParams) -> Vec<u16> {
+    let mut output = vec![0u16; input.len()];
     let (low, high) = output.split_at_mut(input.len() / 2);
 
     low_pass_filter(low, input);
     high_pass_filter(high, low, input, filter);
 
     output
+}
+
+fn low_pass_filter(data: &mut [u16], input: &[u16]) {
+    let compute = |n1: usize, n2: usize| (input[n1] + input[n2]) / 2;
+    let (last, elements) = data.split_last_mut().unwrap();
+
+    for (idx, el) in elements.iter_mut().enumerate() {
+        let n1 = 2 * idx;
+        let n2 = (2 * idx) + 1;
+        *el = compute(n1, n2);
+    }
+
+    let last_idx = (input.len() - 1) / 2;
+    *last = match input.len() & 1 == 1 {
+        true => *input.last().unwrap(),
+        false => compute(2 * last_idx, (2 * last_idx) + 1),
+    };
+}
+
+fn high_pass_filter(data: &mut [u16], low: &[u16], input: &[u16], filter: &FilterParams) {
+    let compute = |n1: usize, n2: usize| (input[n1] - input[n2]) / 2;
+    let (last, elements) = data.split_last_mut().unwrap();
+
+    for (idx, el) in elements.iter_mut().enumerate() {
+        let n1 = 2 * idx;
+        let n2 = (2 * idx) + 1;
+        *el = compute(n1, n2);
+    }
+
+    let last_idx = (input.len() - 1) / 2;
+    *last = match input.len() & 1 == 1 {
+        true => 0,
+        false => compute(2 * last_idx, (2 * last_idx) + 1),
+    };
+
+    let r = |n: usize| (low[n - 1] - low[n]) as f32;
+    let (alpha_neg, alpha_zero, alpha_one, beta) = filter.to_params();
+    let compute = |n: usize| {
+        let n1 = alpha_neg * r(n - 1);
+        let n2 = alpha_zero * r(n);
+        let n3 = alpha_one * r(n + 1);
+        let n4 = beta * elements[n + 1] as f32;
+
+        (n1 + n2 + n3 - n4 + 0.5) as u16
+    };
+
+    elements[1] -= match filter {
+        FilterParams::C => {
+            let n1 = r(1) / 4.0;
+            let n2 = 3.0 / 4.0 * r(2);
+            let n3 = elements[2] as f32 / 4.0;
+            (n1 + n2 - n3 + 0.5) as u16
+        }
+        _ => compute(1),
+    };
+    elements[0] -= r(1) as u16 / 4;
+
+    //TODO: implement for rest of values. maybe without allocating?
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn low_pass() {
+        let input = [0u16, 2, 3, 5, 6, 11, 2, 112, 552];
+        let mut data = vec![0u16; input.len() / 2];
+        super::low_pass_filter(&mut data, &input);
+    }
 }
