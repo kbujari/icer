9e3b45b4092bba1efc58f1066b0123e35c96299b
Kleidi Bujari <mail@kleidi.ca>
2024-01-19 16:41:59 -0500

    perf: removed any allocation from filter

--- src/wavelet/transform.rs
+++ src/wavelet/transform.rs
@@ -26,9 +26,8 @@ fn lowpass(data: &mut [u16], input: &[u16]) {
 }
 
 fn highpass(data: &mut [u16], low: &[u16], input: &[u16], filter: &FilterParams) {
-    let d = {
+    let _ = {
         let compute = |n: usize| input[2 * n].saturating_sub(input[(2 * n) + 1]);
-        let mut data: Vec<_> = data.to_vec();
         let (last, elements) = data.split_last_mut().unwrap();
 
         elements
@@ -40,18 +39,19 @@ fn highpass(data: &mut [u16], low: &[u16], input: &[u16], filter: &FilterParams)
             true => *input.last().unwrap(),
             false => compute((input.len() - 1) / 2),
         };
-
-        data
     };
 
     let r = |n: usize| (low[n - 1].saturating_sub(low[n])) as f32;
     let (alpha_neg, alpha_zero, alpha_one, beta) = filter.to_params();
 
-    let compute = |n: usize| {
+    // 'data_val' should be the value of data[n + 1], passed as a parameter to force a cheap copy and
+    // avoid allocating in this function.
+    // TODO: Look for idiomatic way to accomplish the same thing
+    let compute = |n: usize, data_val: u16| {
         let n1 = alpha_neg * r(n - 1);
         let n2 = alpha_zero * r(n);
         let n3 = alpha_one * r(n + 1);
-        let n4 = beta * d[n + 1] as f32;
+        let n4 = beta * data_val as f32;
 
         (n1 + n2 + n3 - n4 + 0.5) as u16
     };
@@ -61,23 +61,19 @@ fn highpass(data: &mut [u16], low: &[u16], input: &[u16], filter: &FilterParams)
         FilterParams::C => {
             let n1 = r(1) / 4.0;
             let n2 = 3.0 / 4.0 * r(2);
-            let n3 = d[2] as f32 / 4.0;
+            let n3 = data[2] as f32 / 4.0;
             (n1 + n2 - n3 + 0.5) as u16
         }
-        _ => compute(1),
+        _ => compute(1, data[2]),
     });
 
-    let (last, elements) = data.split_last_mut().unwrap();
-
-    elements
-        .iter_mut()
-        .enumerate()
-        .skip(2)
-        .for_each(|(idx, el)| *el = compute(idx));
+    for idx in 2..(data.len() - 1) {
+        data[idx] = compute(idx, data[idx + 1]);
+    }
 
     let last_idx = (input.len() / 2) - 1;
-    *last = match input.len() & 1 == 1 {
-        true => compute(last_idx),
+    *data.last_mut().unwrap() = match input.len() & 1 == 1 {
+        true => compute(last_idx, data[last_idx + 1]),
         false => r(last_idx) as u16 / 4,
     };
 }
